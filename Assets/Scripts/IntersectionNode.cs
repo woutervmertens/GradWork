4b6e9e2aac7238d2756d4a5ec80274871208cb54
woutermertens <wouter.mertens@student.howest.be>
2016-11-30 18:46:17 +0100

    seperate nodes

---
+++ Assets/Scripts/IntersectionNode.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IntersectionNode : MonoBehaviour {
+
+    public Vector3 Position;
+    public int NrVehiclesCont;
+    public NodeType Type;
+    public float Fitness;
+    public float SpeedLimit;
+    public List<Connection> Connections = new List<Connection>();
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void VehicleEntered(Vehicle v)
+    {
+        //Check if light green else stop
+        //Turn and Go Towards Next StartNode
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetType() == typeof(Vehicle))
+        {
+            VehicleEntered(col.GetComponent<Vehicle>());
+        }
+        else if (col.gameObject.transform.parent.GetType() == typeof(Connection))
+        {
+            bool alreadyExists = false;
+            foreach (var con in Connections)
+            {
+                if (con.GetStartNode() == col.gameObject.GetComponent<Node>() ||
+                    con.GetEndNode() == col.gameObject.GetComponent<Node>())
+                {
+                    alreadyExists = true;
+                    continue;
+                }
+            }
+            if (alreadyExists)
+            {
+                Connections.Add(col.gameObject.transform.parent.GetComponent<Connection>());
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerNode.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnerNode : MonoBehaviour {
+
+    public Vector3 Position;
+    public List<Connection> Connections = new List<Connection>();
+    //List of spawnables
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //Spawn if i want to
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetType() == typeof(Vehicle))
+        {
+            if (col.gameObject.GetComponent<Vehicle>().GetTargetNode() == this.GetComponent<Node>())
+            {
+                Destroy(col.gameObject);
+            }
+        }
+        else if (col.gameObject.transform.parent.GetType() == typeof(Connection))
+        {
+            bool alreadyExists = false;
+            foreach (var con in Connections)
+            {
+                if (con.GetStartNode() == col.gameObject.GetComponent<Node>() ||
+                    con.GetEndNode() == col.gameObject.GetComponent<Node>())
+                {
+                    alreadyExists = true;
+                    continue;
+                }
+            }
+            if (alreadyExists)
+            {
+                Connections.Add(col.gameObject.transform.parent.GetComponent<Connection>());
+            }
+        }
+    }
+}
