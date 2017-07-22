e1afb47f9990bd34e875e3ba994d4f8d5e9e8696
woutervmertens <woutervmertens@gmail.com>
2017-07-22 19:45:08 +0200

    Successfull position following

---
+++ Assets/Scripts/TestFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFollow : MonoBehaviour
+{
+    private float timer = 0.0f;
+
+    public bool reset = false;
+
+    public GSDRoad road;
+
+    private GSDSplineC spline;
+
+    public float speed = 10;
+
+    void Start()
+    {
+        spline = road.GSDSpline;
+    }
+    
+	// Update is called once per frame
+	void Update ()
+	{
+	    timer += Time.deltaTime;
+	    if (reset)
+	    {
+	        reset = false;
+	        timer = 0.0f;
+	    }
+
+	    float posAlongSpline = timer/spline.distance;
+	    transform.position = spline.GetSplineValue((posAlongSpline*speed)%1);
+	}
+}
