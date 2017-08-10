2956c18059086f2eb6c72318b250d894f636d2a2
woutervmertens <woutervmertens@gmail.com>
2017-08-10 14:38:01 +0200

    Datainterface

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    //Datainterfaces
+    public class RoadData
+    {
+        public List<Vehicle> PosVehicles { get; private set; }
+        public List<Vehicle> NegVehicles { get; private set; }
+        public float MaxSpeed { get; private set; }
+        public int NrOfLanes { get; private set; }
+
+
+    }
+
+    public class VehicleData
+    {
+        public float MaxSpeed { get; private set; }
+        public float AccelerationSpeed { get; private set; }
+        public float BreakSpeed { get; private set; }
+        public float Length { get; private set; }
+        public float Width { get; private set; }
+        public float ChangeSpeed { get; private set; }
+        public float PreferredLane { get; private set; }
+        public float BufferLength { get; private set; }
+    }
+
+    public class Node
+    {
+        public Vector3 Pos { get; private set; }
+        public bool IsIntersection { get; private set; }
+    }
+
+    public class PathNodes
+    {
+        public Node StartNode { get; private set; }
+        public float StartF { get; private set; }
+        public Node EndNode { get; private set; }
+        public float EndF { get; private set; }
+        public GSDSplineC Spline { get; private set; }
+        public RoadData RoadData { get; private set; }
+        public int Direction { get; private set; }
+    }
+}
--- Assets/Scripts/Vehicles/Behaviors/VehicleBehavior.cs
+++ Assets/Scripts/Vehicles/Behaviors/VehicleBehavior.cs
@@ -8,15 +8,16 @@ namespace Assets.Scripts.Vehicles.Behaviors
     {
         //GENERAL DATA
         //------------
-        //CarData
-        public float MaxSpeed = 100.0f;
-
+        //VehicleData
+        public VehicleData Data;
         public float Speed;
         public int CurrentLane = 0;
-        public float Length = 2.5f;
-        public float BufferLength = 0.5f;
-        public float Acceleration;
-        public float Deceleration;
+
+        //PathData
+        private LinkedList<PathNodes> _path = new LinkedList<PathNodes>();
+        private float _currentSplinePos;
+        public int PathNodeIndex = 0;
+        private Node _startNode, _endNode;
 
         //BEHAVIORS
         //---------
@@ -45,18 +46,18 @@ namespace Assets.Scripts.Vehicles.Behaviors
         {
             if (CurrentLane > 1)
             {
-                Connection con = MainManager.Main.GetCon(RoadPath[RoadNodeIndex]);
-                float vehLength = GetComponent<Vehicle>().GetVehicleLength();
-                float laneWidth = con.LaneWidth;
-                float dist = Mathf.Sqrt((vehLength * vehLength) + (laneWidth * laneWidth));
-                List<Transform> nrNBors = new List<Transform>();
-                foreach (Vehicle neighbour in con.Vehicles)
-                {
-                    if (neighbour == null) continue;
-                    if (dist + BufferLength > Vector3.Distance(transform.position, neighbour.transform.position))
-                        nrNBors.Add(neighbour.transform);
-                }
-                if (nrNBors.Count > 0) return GetComponent<Vehicle>().CheckLeftLane(nrNBors, laneWidth);
+                //Connection con = MainManager.Main.GetCon(RoadPath[RoadNodeIndex]);
+                //float vehLength = GetComponent<Vehicle>().GetVehicleLength();
+                //float laneWidth = con.LaneWidth;
+                //float dist = Mathf.Sqrt((vehLength * vehLength) + (laneWidth * laneWidth));
+                //List<Transform> nrNBors = new List<Transform>();
+                //foreach (Vehicle neighbour in con.Vehicles)
+                //{
+                //    if (neighbour == null) continue;
+                //    if (dist + BufferLength > Vector3.Distance(transform.position, neighbour.transform.position))
+                //        nrNBors.Add(neighbour.transform);
+                //}
+                //if (nrNBors.Count > 0) return GetComponent<Vehicle>().CheckLeftLane(nrNBors, laneWidth);
                 return true;
             }
             return false;
@@ -64,41 +65,41 @@ namespace Assets.Scripts.Vehicles.Behaviors
 
         public bool CheckRightLane()
         {
-            if (CurrentLane < MainManager.Main.GetCon(RoadPath[RoadNodeIndex]).NrOfLanes)
-            {
-                Connection con = MainManager.Main.GetCon(RoadPath[RoadNodeIndex]);
-                float vehLength = GetComponent<Vehicle>().GetVehicleLength();
-                float laneWidth = con.LaneWidth;
-                float dist = Mathf.Sqrt((vehLength * vehLength) + (laneWidth * laneWidth));
-                List<Transform> nrNBors = new List<Transform>();
-                foreach (Vehicle neighbour in con.Vehicles)
-                {
-                    if (neighbour == null) continue;
-                    if (dist + BufferLength > Vector3.Distance(transform.position, neighbour.transform.position))
-                        nrNBors.Add(neighbour.transform);
-                }
-                if (nrNBors.Count > 0) return GetComponent<Vehicle>().CheckLeftLane(nrNBors, laneWidth);
-                return true;
-            }
+            //if (CurrentLane < MainManager.Main.GetCon(RoadPath[RoadNodeIndex]).NrOfLanes)
+            //{
+            //    Connection con = MainManager.Main.GetCon(RoadPath[RoadNodeIndex]);
+            //    float vehLength = GetComponent<Vehicle>().GetVehicleLength();
+            //    float laneWidth = con.LaneWidth;
+            //    float dist = Mathf.Sqrt((vehLength * vehLength) + (laneWidth * laneWidth));
+            //    List<Transform> nrNBors = new List<Transform>();
+            //    foreach (Vehicle neighbour in con.Vehicles)
+            //    {
+            //        if (neighbour == null) continue;
+            //        if (dist + BufferLength > Vector3.Distance(transform.position, neighbour.transform.position))
+            //            nrNBors.Add(neighbour.transform);
+            //    }
+            //    if (nrNBors.Count > 0) return GetComponent<Vehicle>().CheckLeftLane(nrNBors, laneWidth);
+            //    return true;
+            //}
             return false;
         }
 
         private bool CheckCloseByNeighbours(float buffer)
         {
-            if (IsOnIntersection) return false;
-            foreach (Vehicle neighbour in MainManager.Main.GetCon(RoadPath[RoadNodeIndex]).Vehicles)
-            {
-                if (neighbour == null || neighbour == this.GetComponent<Vehicle>()) continue;
-                if ((Mathf.Abs(Vector3.Distance(neighbour.transform.position, this.transform.position)) < _detectionLength * Speed)
-                    && (neighbour.GetComponent<UnitBehaviorTree>().Lane == CurrentLane))
-                {
-                    _neighbourDistance = Mathf.Abs(Vector3.Distance(neighbour.transform.position, this.transform.position));
-                    Vector3 fwdPos = transform.TransformPoint(Vector3.forward * (_detectionLength * Speed));
-                    float distFromHereToNB = Vector3.Distance(transform.position, neighbour.transform.position);
-                    float distFromFwdPosToNB = Vector3.Distance(fwdPos, neighbour.transform.position);
-                    return (distFromHereToNB < distFromFwdPosToNB && distFromFwdPosToNB < (_detectionLength * Speed) && Speed > neighbour.Speed);//NB is between this and fwdPos and is slower
-                }
-            }
+            //if (IsOnIntersection) return false;
+            //foreach (Vehicle neighbour in MainManager.Main.GetCon(RoadPath[RoadNodeIndex]).Vehicles)
+            //{
+            //    if (neighbour == null || neighbour == this.GetComponent<Vehicle>()) continue;
+            //    if ((Mathf.Abs(Vector3.Distance(neighbour.transform.position, this.transform.position)) < _detectionLength * Speed)
+            //        && (neighbour.GetComponent<UnitBehaviorTree>().Lane == CurrentLane))
+            //    {
+            //        _neighbourDistance = Mathf.Abs(Vector3.Distance(neighbour.transform.position, this.transform.position));
+            //        Vector3 fwdPos = transform.TransformPoint(Vector3.forward * (_detectionLength * Speed));
+            //        float distFromHereToNB = Vector3.Distance(transform.position, neighbour.transform.position);
+            //        float distFromFwdPosToNB = Vector3.Distance(fwdPos, neighbour.transform.position);
+            //        return (distFromHereToNB < distFromFwdPosToNB && distFromFwdPosToNB < (_detectionLength * Speed) && Speed > neighbour.Speed);//NB is between this and fwdPos and is slower
+            //    }
+            //}
             return false;
         }
 
@@ -121,19 +122,19 @@ namespace Assets.Scripts.Vehicles.Behaviors
 
         public BehaviorState SpeedUp()
         {
-            if (Speed < (MainManager.Main.GetCon(RoadPath[RoadNodeIndex]).MaxSpeed) * ((100 - (5 * (CurrentLane - 1))) / 100))
-            {
-                Speed += Acceleration * Time.deltaTime;
-                return BehaviorState.Running;
-            }
+            //if (Speed < (MainManager.Main.GetCon(RoadPath[RoadNodeIndex]).MaxSpeed) * ((100 - (5 * (CurrentLane - 1))) / 100))
+            //{
+            //    Speed += Acceleration * Time.deltaTime;
+            //    return BehaviorState.Running;
+            //}
             return BehaviorState.Success;
         }
 
         public BehaviorState SlowDown()
         {
             //slow down untill ray is false
-            Speed -= (Deceleration * (_detectionLength * Speed - _neighbourDistance) * Time.deltaTime);
-            if (CheckCloseByNeighbours(BufferLength)) return BehaviorState.Running;
+            //Speed -= (Deceleration * (_detectionLength * Speed - _neighbourDistance) * Time.deltaTime);
+            if (CheckCloseByNeighbours(Data.BufferLength)) return BehaviorState.Running;
             return BehaviorState.Success;
         }
 
