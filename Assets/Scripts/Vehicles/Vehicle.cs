d1b6b915adc4499dabc47d484310e400d968d4d7
woutermertens <wouter.mertens@student.howest.be>
2017-08-13 16:49:30 +0200

    unity get's stuck

--- Assets/Scripts/Vehicles/Vehicle.cs
+++ Assets/Scripts/Vehicles/Vehicle.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vehicle : MonoBehaviour {
-
+public class Vehicle : MonoBehaviour
+{
+    private List<PathMapBuilder.PathObject> Path;
 	// Use this for initialization
 	void Start () {
 		
