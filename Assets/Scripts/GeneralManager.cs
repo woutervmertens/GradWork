60915fdc576861e5f45e8d5c291f3b1f3d2a6dbb
woutermertens <wouter.mertens@student.howest.be>
2017-08-19 22:00:17 +0200

    CameraScript and FPS count

--- Assets/Scripts/GeneralManager.cs
+++ Assets/Scripts/GeneralManager.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GeneralManager : MonoBehaviour
 {
 
     public bool DebugMode = false;
+
+    public Text VehicleNr;
+
+    public Text Fps;
 	// Use this for initialization
 	void Start ()
 	{
 	    RoadManager.DebubMode = DebugMode;
 	}
+
+    void Update()
+    {
+        VehicleNr.text = ("#Vehicles: " + RoadManager.NumberOfVehicles).ToString();
+        Fps.text = ("FPS: " + (int)(1.0 / Time.deltaTime)).ToString();
+    }
 }
